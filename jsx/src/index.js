a87be7d8b30baa1a8cb8080767fa54098e989d54
Beibarys Learner <beibarys.learner@gmail.com>
2024-04-28 21:57:08 +0500

    new app component

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const el  = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
-
-function App() {
-    return <h1>Salem React!</h1>;
-}
 
 root.render(<App />);
